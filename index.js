1d6edc87d071ecce4dd18ad7d4bca511447d2c3d
Zena Hirsch <hirsch.zena@gmail.com>
2016-02-15 22:17:50 -0500

    fix js dependencies

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var Handlebars = require("handlebars");
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+    var js = fs.readFileSync(__dirname + "/js/resume.js", "utf-8");
 
 	return Handlebars.compile(tpl)({
-		css: css,
-		resume: resume
-	});
+		css: css, 
+		resume: resume,
+        js: js
+	});    
 }
 
 module.exports = {
